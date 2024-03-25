53c6a73450ed6a3a77ff4c9b7863e7a2c85a68f5
ngshenmeng2401 <55821381+ngshenmeng2401@users.noreply.github.com>
2024-03-25 17:03:34 +0800

    Add files via upload

---
+++ emoji_keys.dart
@@ -0,0 +1,34 @@
+enum ReactionEmoji {
+  nothing,
+  like,
+  love,
+  haha,
+  wow,
+  sad,
+  angry,
+}
+
+class AssetSounds {
+  static const String boxDown = "assets/emoji_sounds/sound_box_down.mp3";
+  static const String boxUp = "assets/emoji_sounds/sound_box_up.mp3";
+  static const String focus = "assets/emoji_sounds/sound_focus.mp3";
+  static const String pick = "assets/emoji_sounds/sound_pick.mp3";
+  static const String shortPressLike = "assets/emoji_sounds/sound_short_press_like.mp3";
+}
+
+class AssetImages {
+  static const String loveGif = "assets/emoji_images/love.gif";
+  static const String hahaGif = "assets/emoji_images/haha.gif";
+  static const String likeGif = "assets/emoji_images/like.gif";
+  static const String wowGif = "assets/emoji_images/wow.gif";
+  static const String sadGif = "assets/emoji_images/sad.gif";
+  static const String angryGif = "assets/emoji_images/angry.gif";
+
+  static const String icLikeFill = "assets/emoji_images/ic_like_fill.png";
+  static const String icLike = "assets/emoji_images/ic_like.png";
+  static const String icLove2 = "assets/emoji_images/love2.png";
+  static const String icHaha2 = "assets/emoji_images/haha2.png";
+  static const String icWow2 = "assets/emoji_images/wow2.png";
+  static const String icSad2 = "assets/emoji_images/sad2.png";
+  static const String icAngry2 = "assets/emoji_images/angry2.png";
+}
